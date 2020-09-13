4c602be0fcb4050cd9da77a2b3bf9a01dc4dbe46
Caleb Evans <caleb@calebevans.me>
2020-09-13 09:25:00 -0700

    Print total number of GIFs

--- ica/analyzer.py
+++ ica/analyzer.py
@@ -37,14 +37,33 @@ def get_messages_dataframe(phone_number):
             })
 
 
+# Return a pandas dataframe representing all attachments in a particular
+# conversation (identified by the given phone number)
+def get_attachments_dataframe(phone_number):
+
+    with sqlite3.connect(get_db_path()) as connection:
+        return pandas.read_sql_query(
+            sql=get_sql_query('attachments'),
+            con=connection,
+            params={
+                'phone_number': phone_number
+            })
+
+
 def run(phone_number):
 
     messages_dataframe = get_messages_dataframe(phone_number)
+    attachments_dataframe = get_attachments_dataframe(phone_number)
+
     total_message_count = len(messages_dataframe.index)
     if not total_message_count:
         print('Conversation not found', file=sys.stderr)
         sys.exit(1)
+
+    total_gif_count = attachments_dataframe.mime_type.eq('image/gif').sum()
+
     print(f'{total_message_count:,} total messages!')
+    print(f'{total_gif_count:,} total GIFs!')
     # for index, row in messages_dataframe.iterrows():
     #     print(row)
     #     print('')
