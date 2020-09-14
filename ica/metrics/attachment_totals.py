4fed65d2f7cb34241256c41d54b7113f06cba830
Caleb Evans <caleb@calebevans.me>
2020-09-14 08:59:07 -0700

    Fix 'attachment totals' metrics module

--- ica/metrics/attachment_totals.py
+++ ica/metrics/attachment_totals.py
@@ -6,7 +6,11 @@ import pandas as pd
 
 def analyze(dfs):
 
-    return pd.DataFrame({
+    totals_map = {
         'gifs': dfs.attachments['mime_type'].eq('image/gif').sum(),
         'videos': dfs.attachments['mime_type'].eq('video/quicktime').sum()
+    }
+    return pd.DataFrame({
+        'type': totals_map.keys(),
+        'total': totals_map.values()
     })
