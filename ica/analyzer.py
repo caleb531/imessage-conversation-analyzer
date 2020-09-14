c6611c9eddd53cb11fa17e2e22d38ecb889c310b
Caleb Evans <caleb@calebevans.me>
2020-09-13 17:35:09 -0700

    Consistently use ['text'] instead of .text

--- ica/analyzer.py
+++ ica/analyzer.py
@@ -81,7 +81,7 @@ def analyze_conversation(phone_number):
     # Output the occurrences of specific emojis
     most_frequent_emojis = pd.DataFrame({
         'emoji': EMOJIS,
-        'count': [dataframes.messages.text.str.extract('(' + emoji + ')')
+        'count': [dataframes.messages['text'].str.extract('(' + emoji + ')')
                   .count().item() for emoji in EMOJIS]
     }, columns=['emoji', 'count'])
     print(most_frequent_emojis.sort_values(by='count', ascending=False))
@@ -89,7 +89,7 @@ def analyze_conversation(phone_number):
     # Copy the messages dataframe so that we can count all "text" column values
     # by converting them to integers (always 1)
     messages_aggregate = dataframes.messages.copy()
-    messages_aggregate['text'] = messages_aggregate.text.apply(
+    messages_aggregate['text'] = messages_aggregate['text'].apply(
         pd.to_numeric, errors='coerce').isna()
     groups_by_day = messages_aggregate.resample('D', on='datetime')
     sums_by_day = groups_by_day.sum()
