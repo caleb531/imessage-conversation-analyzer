3367f2c234835d18586e253de2d793e9666ad1eb
Caleb Evans <caleb@calebevans.me>
2020-09-13 20:18:54 -0700

    Store most popular emojis in text file

--- ica/metrics/most_frequent_emojis.py
+++ ica/metrics/most_frequent_emojis.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
+import collections
 import pandas as pd
 
 
-# The respective emojis to count across the entire iMessage conversation
-EMOJIS = ('❤️', '😍', '😘', '🥰', '😊', '😂', '😅', '🌙')
+# Fetch a list of the most popular emoji on the Web to use as a basis for
+# computing this metric
+def get_emoji_list():
+
+    with open('ica/emojis.txt', 'r', encoding='utf-8') as emojis_file:
+        # Filter out duplicates
+        return tuple(collections.OrderedDict.fromkeys(emojis_file.read()))
 
 
+# Output the occurrences of specific emojis
 def analyze(dfs):
 
-    # Output the occurrences of specific emojis
+    emojis = get_emoji_list()
+    print(emojis)
+    return
     most_frequent_emojis = pd.DataFrame({
-        'emoji': EMOJIS,
+        'emoji': emojis,
         'count': [dfs.messages['text'].str.extract('(' + emoji + ')')
-                  .count().item() for emoji in EMOJIS]
+                  .count().item() for emoji in emojis]
     })
-
     return most_frequent_emojis.sort_values(by='count', ascending=False)
