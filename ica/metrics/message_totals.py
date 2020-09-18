c99333c4abac6b51555747d5c4759459372d10f7
Caleb Evans <caleb@calebevans.me>
2020-09-17 18:19:19 -0700

    Count number of days missed

--- ica/metrics/message_totals.py
+++ ica/metrics/message_totals.py
@@ -5,23 +5,49 @@ import datetime
 import pandas as pd
 
 
+# The format to use for all date strings
+DATE_FORMAT = '%Y-%m-%d'
+
+
+# Retrieve the date of the very first message sent in the conversation
+def get_first_message_date(dfs):
+
+    datestr = str(dfs.messages['datetime'].min().strftime(DATE_FORMAT))
+    return datetime.datetime.strptime(datestr, DATE_FORMAT)
+
+
 # Get all dates between (and including) the given two dates
 def get_dates_between(start_date, end_date):
 
-    delta = end_date - start_date
-    dates = []
-    for d in range(delta.days + 1):
-        date = start_date + datetime.timedelta(days=d)
-        dates.append(date)
-    return dates
+    return list(pd.date_range(start_date, end_date).strftime(DATE_FORMAT))
+
+
+# Get all dates sent
+def get_all_message_datestrs(dfs):
+
+    groups_by_day = dfs.messages.resample('D', on='datetime')
+    sums_by_day = groups_by_day.sum()
+    sums_by_day.index = sums_by_day.index.strftime(DATE_FORMAT)
+    return list(sums_by_day.index)
+
+
+# Return a list of all dates in Date List B which are not in Date List A
+def get_missing_datestrs(date_list_a, date_list_b):
+
+    return sorted(set(date_list_a) - set(date_list_b))
 
 
 def analyze(dfs):
 
+    all_datestrs = get_dates_between(
+        get_first_message_date(dfs), str(datetime.date.today()))
+    message_datestrs = get_all_message_datestrs(dfs)
+
     totals_map = {
         'messages': len(dfs.messages.index),
         'messages_from_me': dfs.messages['is_from_me'].eq(True).sum(),
-        'messages_from_them': dfs.messages['is_from_me'].eq(False).sum()
+        'messages_from_them': dfs.messages['is_from_me'].eq(False).sum(),
+        'days_missed': len(all_datestrs) - len(message_datestrs)
     }
     return pd.DataFrame({
         'metric': tuple(totals_map.keys()),
--- ica/metrics/totals_by_day.py
+++ ica/metrics/totals_by_day.py
@@ -4,16 +4,19 @@
 import pandas as pd
 
 
+# The format to use for all date strings
+DATE_FORMAT = '%Y-%m-%d'
+
+
 def analyze(dfs):
 
-    # Copy the messages dataframe so that we can count all "text" column values
-    # by converting them to integers (always 1)
+    # Count all "text" column values by converting them to integers (always 1)
     dfs.messages['text'] = dfs.messages['text'].apply(
         pd.to_numeric, errors='coerce').isna()
     groups_by_day = dfs.messages.resample('D', on='datetime')
     sums_by_day = groups_by_day.sum()
     # Remove 00:00:00 from date index
-    sums_by_day.index = sums_by_day.index.strftime('%Y-%m-%d')
+    sums_by_day.index = sums_by_day.index.strftime(DATE_FORMAT)
     sums_by_day['is_from_them'] = (sums_by_day['text']
                                    - sums_by_day['is_from_me'])
     return sums_by_day
