90ef2bc7d21482da7fc6debdf9eef7a5d54f9ffa
Caleb Evans <caleb@calebevans.me>
2020-09-20 11:10:00 -0700

    Correct "handles" to "messages" in comment

--- ica/queries/attachments.sql
+++ ica/queries/attachments.sql
@@ -1,6 +1,6 @@
 -- Get all attachments (GIFs, images, links, etc.)
 SELECT "mime_type" FROM "attachment" INNER JOIN "message_attachment_join" ON "attachment"."ROWID" = "attachment_id" WHERE "message_id" IN (
-    -- Get all handles tied to chat
+    -- Get all messages tied to chat
     SELECT "message_id" FROM "chat_message_join" WHERE "chat_id" IN (
         -- Get all chats with the specified phone number
         SELECT "chat"."ROWID" FROM "chat" WHERE "chat_identifier" LIKE ('%' || :phone_number)
