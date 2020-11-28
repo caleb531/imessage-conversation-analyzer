5f4c465969240636c8576ff2cef0a2998421fb27
Caleb Evans <caleb@calebevans.me>
2020-11-27 17:04:41 -0800

    Track days where there were no messages from other person

--- ica/metrics/message_totals.py
+++ ica/metrics/message_totals.py
@@ -31,6 +31,22 @@ def get_all_message_datestrs(dfs):
     return list(sums_by_day.index)
 
 
+# Get the number of messages with no reply (i.e. only one message sent for that
+# day)
+def get_noreply_count(dfs):
+
+    # Count all "text" column values by converting them to integers (always 1),
+    # because resampling the DataFrame will remove all non-numeric columns
+    dfs.messages['text'] = dfs.messages['text'].apply(
+        pd.to_numeric, errors='coerce').isna()
+    groups_by_day = dfs.messages.resample('D', on='datetime')
+    sums_by_day = groups_by_day.sum()
+    sums_by_day['is_from_them'] = (sums_by_day['text']
+                                   - sums_by_day['is_from_me'])
+    return (sums_by_day['is_from_them'].eq(0).sum()
+            + sums_by_day['is_from_me'].eq(0).sum())
+
+
 # Return a list of all dates in Date List B which are not in Date List A
 def get_missing_datestrs(date_list_a, date_list_b):
 
@@ -47,6 +63,7 @@ def analyze(dfs):
         'messages': len(dfs.messages.index),
         'messages_from_me': dfs.messages['is_from_me'].eq(True).sum(),
         'messages_from_them': dfs.messages['is_from_me'].eq(False).sum(),
+        'no_reply': get_noreply_count(dfs),
         'days_messaged': len(message_datestrs),
         'days_missed': len(all_datestrs) - len(message_datestrs)
     }
