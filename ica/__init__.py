745314428337949d7b67c459b3ede1a85999189e
Caleb Evans <caleb@calebevans.me>
2024-01-23 12:51:11 -0800

    Ignore flake8 for entire ica init file

--- ica/__init__.py
+++ ica/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# flake8: noqa
 
-from ica.cli import get_cli_args  # noqa: F401
-from ica.core import DataFrameNamespace, get_dataframes, output_results  # noqa: F401
+from ica.cli import get_cli_args
+from ica.core import DataFrameNamespace, get_dataframes, output_results
