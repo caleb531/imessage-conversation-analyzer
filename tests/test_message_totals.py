7f469d27650b618bb967116a611be068258cc3f0
Caleb Evans <caleb@calebevans.me>
2024-01-25 12:43:50 -0800

    Fix day counts test breaking when current date changes
    We use the freezegun package to ensure that the current date is fixed to
    a constant date/time (at least for this particular test).

--- tests/test_message_totals.py
+++ tests/test_message_totals.py
@@ -4,6 +4,7 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
+from freezegun import freeze_time
 from nose2.tools.decorators import with_setup, with_teardown
 
 import ica.analyzers.message_totals as message_totals
@@ -42,10 +43,11 @@ def test_reaction_counts(output_results: MagicMock) -> None:
 @with_teardown(tear_down)
 @patch("ica.output_results")
 @patch("sys.argv", [message_totals.__file__, "-c", "Jane Doe"])
+@freeze_time("2024-01-26 9:00:00")
 def test_day_counts(output_results: MagicMock) -> None:
     """should count the number of days according to various criteria"""
     message_totals.main()
     df = output_results.call_args[0][0]
     case.assertEqual(df.loc["days_messaged"]["total"], 8)
-    case.assertEqual(df.loc["days_missed"]["total"], 10)
+    case.assertEqual(df.loc["days_missed"]["total"], 12)
     case.assertEqual(df.loc["days_with_no_reply"]["total"], 0)
