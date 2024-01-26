c4797d5541a7dffaf998156b581d40c843fefc38
Caleb Evans <caleb@calebevans.me>
2024-01-26 09:14:41 -0800

    Switch to LEFT JOIN to maximize sqlite3 version compatibility
    See lengthy code comment for details.

--- ica/queries/contact.sql
+++ ica/queries/contact.sql
@@ -2,7 +2,11 @@ SELECT
     "ZFULLNUMBER",
     "ZADDRESS"
 FROM "ZABCDPHONENUMBER"
-FULL OUTER JOIN "ZABCDEMAILADDRESS"
+-- Using a FULL OUTER JOIN would be preferable here, however sqlite3 has only
+-- recently added support for FULL OUTER JOIN, and attempting to upgrade
+-- Python's version of sqlite3 has proven challenging and is fraught with its
+-- own risks; therefore, we opt for simply using a LEFT JOIN instead
+LEFT JOIN "ZABCDEMAILADDRESS"
     ON "ZABCDPHONENUMBER"."ZOWNER" = "ZABCDEMAILADDRESS"."ZOWNER"
 WHERE "ZABCDPHONENUMBER"."ZOWNER" = (
     SELECT "Z_PK"
