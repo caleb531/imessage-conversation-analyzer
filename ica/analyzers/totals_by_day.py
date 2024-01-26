3423302d67cc7e3f0b7aa9807c39e931a0fb1898
Caleb Evans <caleb@calebevans.me>
2024-01-26 20:10:20 +0000

    Use proper DateTimeIndex for Totals By Day index
    This allows for timezone conversion and other date-specific operations
    on the values in the index.

--- ica/analyzers/totals_by_day.py
+++ ica/analyzers/totals_by_day.py
@@ -10,20 +10,22 @@ DATE_FORMAT = "%Y-%m-%d"
 def main() -> None:
     cli_args = ica.get_cli_args()
     dfs = ica.get_dataframes(contact_name=cli_args.contact_name)
-    groups_by_day = (
-        dfs.messages
-        # Count all "text" column values by converting them to integers (always
-        # 1), because resampling the DataFrame will remove all non-numeric
-        # columns
-        .assign(
-            text=lambda df: df["text"].apply(pd.to_numeric, errors="coerce").isna()
-        ).resample("D", on="datetime")
+    ica.output_results(
+        (
+            dfs.messages
+            # Count all "text" column values by converting them to integers (always
+            # 1), because resampling the DataFrame will remove all non-numeric
+            # columns
+            .assign(
+                text=lambda df: df["text"].apply(pd.to_numeric, errors="coerce").isna()
+            )
+            .resample("D", on="datetime")
+            .sum()
+            .pipe(lambda df: df.rename_axis("date", axis=0))
+            .assign(is_from_them=lambda df: df["text"] - df["is_from_me"])
+        ),
+        format=cli_args.format,
     )
-    sums_by_day = groups_by_day.sum()
-    # Remove 00:00:00 from date index
-    sums_by_day.index = sums_by_day.index.strftime(DATE_FORMAT).rename("date")
-    sums_by_day["is_from_them"] = sums_by_day["text"] - sums_by_day["is_from_me"]
-    ica.output_results(sums_by_day, format=cli_args.format)
 
 
 if __name__ == "__main__":
