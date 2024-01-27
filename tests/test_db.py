50ca29181d68e3f1e9c4ac47658f77eab3595452
Caleb Evans <caleb@calebevans.me>
2024-01-27 09:22:39 -0800

    Improve tests for mock database paths

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
 """test the mocking of the database"""
 
+import glob
 import sqlite3
 import unittest
 
 from nose2.tools.decorators import with_setup, with_teardown
 
-import ica.core
-from tests import mock_chats_db_path, set_up, tear_down
+import ica.contact as contact
+import ica.core as core
+from tests import (
+    mock_chats_db_path,
+    mock_contacts_db_glob,
+    mock_contacts_db_path,
+    set_up,
+    tear_down,
+)
 
 case = unittest.TestCase()
 
 
 @with_setup(set_up)
 @with_teardown(tear_down)
-def test_db_path() -> None:
-    """should mock path to database"""
-    case.assertEqual(ica.core.DB_PATH, mock_chats_db_path)
+def test_db_paths() -> None:
+    """should mock paths to databases"""
+    case.assertEqual(core.DB_PATH, mock_chats_db_path)
+    case.assertEqual(contact.DB_GLOB, mock_contacts_db_glob)
+    case.assertIn(
+        mock_contacts_db_path,
+        glob.glob(mock_contacts_db_glob),
+        "glob for mock contacts database does not correctly resolve to database path",
+    )
 
 
 @with_setup(set_up)
