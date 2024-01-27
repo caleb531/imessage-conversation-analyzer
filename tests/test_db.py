6f1a796d3a7b7f62e79a946baea907cb2ae1387e
Caleb Evans <caleb@calebevans.me>
2024-01-27 09:17:21 -0800

    Add test for foreign keys in mock chat database

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """test the mocking of the database"""
 
+import sqlite3
 import unittest
 
 from nose2.tools.decorators import with_setup, with_teardown
@@ -16,3 +17,21 @@ case = unittest.TestCase()
 def test_db_path() -> None:
     """should mock path to database"""
     case.assertEqual(ica.core.DB_PATH, mock_chats_db_path)
+
+
+@with_setup(set_up)
+@with_teardown(tear_down)
+def test_chat_db_foreign_keys() -> None:
+    """foreign key associations for mock chat database should be correct"""
+    with sqlite3.connect(mock_chats_db_path) as con:
+        cur = con.cursor()
+        message_ids = set(
+            row[0] for row in cur.execute("SELECT ROWID FROM message").fetchall()
+        )
+        joined_message_ids = set(
+            row[0]
+            for row in cur.execute(
+                "SELECT message_id FROM chat_message_join"
+            ).fetchall()
+        )
+        case.assertEqual(joined_message_ids, message_ids)
