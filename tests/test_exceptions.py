06078108f56206d1d5c628273d8af95b355ce4eb
Caleb Evans <caleb@calebevans.me>
2024-01-27 09:40:57 -0800

    Add test for explicit raising of ContactNotFoundError

---
+++ tests/test_exceptions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""test the message_totals built-in analyzer"""
+
+import unittest
+
+from nose2.tools.decorators import with_setup, with_teardown
+
+import ica
+from tests import set_up, tear_down
+
+case = unittest.TestCase()
+
+
+@with_setup(set_up)
+@with_teardown(tear_down)
+def test_contact_not_found_error() -> None:
+    """should raise a ContactNotFoundError when a contact is not found"""
+    with case.assertRaises(ica.ContactNotFoundError):
+        ica.get_dataframes(contact_name="Imaginary Person")
