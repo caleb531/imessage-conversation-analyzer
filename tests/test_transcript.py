12c7aad49cb65db2b41f18c8259f5b1afc3f201f
Caleb Evans <caleb@calebevans.me>
2024-01-27 17:54:46 -0800

    Build out transcript tests
    The transcript tests have been improved in several ways:
    - A transcript has been added for the second mock conversation, which
      covers the remaining reaction types
    - Now that timezone information is included in the transcript output,
      the transcript JSON (for both conversations) has been updated to use
      timezone-aware timestamps; this enables us to eliminate the
      tz_localize timezone logic within the tests themselves, thus
      simplifying the code (even a little)

--- tests/test_transcript.py
+++ tests/test_transcript.py
@@ -17,18 +17,25 @@ case = unittest.TestCase()
 @with_teardown(tear_down)
 @patch("ica.output_results")
 @patch("sys.argv", [transcript.__file__, "-c", "Jane Doe", "-t", "UTC"])
-def test_transcript(output_results: MagicMock) -> None:
-    """should generate a transcript of an entire conversation"""
+def test_transcript_1(output_results: MagicMock) -> None:
+    """should generate a transcript of the first mock conversation"""
     transcript.main()
     df: pd.DataFrame = output_results.call_args[0][0]
     case.assertListEqual(
         df.to_dict(orient="records"),
-        pd.read_json("tests/data/transcript.json")
-        # ICA provides timezone-aware date/times, however the date/time objects
-        # parsed from the JSON are missing timezone information (i.e. they are
-        # timezone-naive); therefore, we must add the missing timezone
-        # information (note that this does not perform any conversions)
-        .assign(timestamp=lambda df: df["timestamp"].dt.tz_localize("UTC")).to_dict(
-            orient="records"
-        ),
+        pd.read_json("tests/data/transcript-1.json").to_dict(orient="records"),
+    )
+
+
+@with_setup(set_up)
+@with_teardown(tear_down)
+@patch("ica.output_results")
+@patch("sys.argv", [transcript.__file__, "-c", "James Doe", "-t", "UTC"])
+def test_transcript_2(output_results: MagicMock) -> None:
+    """should generate a transcript of the second mock conversation"""
+    transcript.main()
+    df: pd.DataFrame = output_results.call_args[0][0]
+    case.assertListEqual(
+        df.to_dict(orient="records"),
+        pd.read_json("tests/data/transcript-2.json").to_dict(orient="records"),
     )
