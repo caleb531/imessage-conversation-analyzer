bd6828f9aef8ae75cbc82b97606c41e2174471dc
Caleb Evans <caleb@calebevans.me>
2024-01-29 15:15:09 -0800

    Use nose2 parameterization to eliminate redundant transcript test

--- tests/test_transcript.py
+++ tests/test_transcript.py
@@ -2,9 +2,11 @@
 """test the transcript built-in analyzer"""
 
 import unittest
+from typing import Any
 from unittest.mock import MagicMock, patch
 
 import pandas as pd
+from nose2.tools import params
 from nose2.tools.decorators import with_setup, with_teardown
 
 import ica.analyzers.transcript as transcript
@@ -13,29 +15,20 @@ from tests import set_up, tear_down
 case = unittest.TestCase()
 
 
+@params((1, "Jane Doe"), (2, "James Doe"))
 @with_setup(set_up)
 @with_teardown(tear_down)
 @patch("ica.output_results")
-@patch("sys.argv", [transcript.__file__, "-c", "Jane Doe", "-t", "UTC"])
-def test_transcript_1(output_results: MagicMock) -> None:
-    """should generate a transcript of the first mock conversation"""
-    transcript.main()
-    df: pd.DataFrame = output_results.call_args[0][0]
-    case.assertListEqual(
-        df.to_dict(orient="records"),
-        pd.read_json("tests/data/transcript-1.json").to_dict(orient="records"),
-    )
-
-
-@with_setup(set_up)
-@with_teardown(tear_down)
-@patch("ica.output_results")
-@patch("sys.argv", [transcript.__file__, "-c", "James Doe", "-t", "UTC"])
-def test_transcript_2(output_results: MagicMock) -> None:
-    """should generate a transcript of the second mock conversation"""
-    transcript.main()
-    df: pd.DataFrame = output_results.call_args[0][0]
-    case.assertListEqual(
-        df.to_dict(orient="records"),
-        pd.read_json("tests/data/transcript-2.json").to_dict(orient="records"),
-    )
+def test_transcripts(
+    transcript_num: Any, contact_name: str, output_results: MagicMock
+) -> None:
+    """should generate transcripts of all mock conversations"""
+    with patch("sys.argv", [transcript.__file__, "-c", contact_name, "-t", "UTC"]):
+        transcript.main()
+        df: pd.DataFrame = output_results.call_args[0][0]
+        case.assertListEqual(
+            df.to_dict(orient="records"),
+            pd.read_json(f"tests/data/transcript-{transcript_num}.json").to_dict(
+                orient="records"
+            ),
+        )
