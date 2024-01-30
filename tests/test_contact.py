b0de86ded51ad26ff12fde1467d8a07bfaaeacb1
Caleb Evans <caleb@calebevans.me>
2024-01-29 20:55:41 -0800

    Add test for contact existing but missing phone number

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -13,6 +13,17 @@ case = unittest.TestCase()
 
 @with_setup(set_up)
 @with_teardown(tear_down)
+def test_missing_phone_number() -> None:
+    """
+    should raise a ContactNotFoundError if contact exists but has no phone
+    number on record
+    """
+    with case.assertRaises(ica.ContactNotFoundError):
+        ica.get_dataframes(contact_name="Jackson Doe")
+
+
+@with_setup(set_up)
+@with_teardown(tear_down)
 def test_contact_not_found_error() -> None:
     """should raise a ContactNotFoundError when a contact is not found"""
     with case.assertRaises(ica.ContactNotFoundError):
