478b3a31a38cbf34e672baad0a0d7833be6aae1b
Caleb Evans <caleb@calebevans.me>
2024-01-30 16:36:16 -0800

    Export BaseAnalyzerException at the package level
    This allows BaseAnalyzerException to be imported by consuming
    applications as part of the `ica` package namespace.

--- ica/__init__.py
+++ ica/__init__.py
@@ -3,4 +3,8 @@
 
 from ica.cli import get_cli_args
 from ica.core import DataFrameNamespace, get_dataframes, output_results
-from ica.exceptions import ContactNotFoundError, ConversationNotFoundError
+from ica.exceptions import (
+    BaseAnalyzerException,
+    ContactNotFoundError,
+    ConversationNotFoundError,
+)
