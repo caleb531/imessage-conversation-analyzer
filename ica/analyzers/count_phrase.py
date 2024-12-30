0af61006d7c3a82cd41888226b99607120a0cd88
Caleb Evans <caleb@calebevans.me>
2024-12-30 12:21:48 -0800

    Allow count_phrase analyzer to accept more than one phrase

--- ica/analyzers/count_phrase.py
+++ ica/analyzers/count_phrase.py
@@ -10,16 +10,27 @@ import ica
 def main() -> None:
 
     cli_args = ica.get_cli_args()
-    phrase = cli_args.args[0]
-    if not phrase:
-        raise Exception("Phrase to count must be provided")
+    phrases = cli_args.args
+    if not phrases:
+        raise Exception("Must provide at least one phrase to count")
     dfs = ica.get_dataframes(
         contact_name=cli_args.contact_name, timezone=cli_args.timezone
     )
     filtered_messages = dfs.messages[~dfs.messages["is_reaction"]]
-    count = filtered_messages["text"].str.count(phrase, flags=re.IGNORECASE).sum()
     ica.output_results(
-        (pd.DataFrame({"phrase": (phrase,), "count": (count,)}).set_index("phrase")),
+        (
+            pd.DataFrame(
+                {
+                    "phrase": phrases,
+                    "count": (
+                        filtered_messages["text"]
+                        .str.count(phrase, flags=re.IGNORECASE)
+                        .sum()
+                        for phrase in phrases
+                    ),
+                }
+            ).set_index("phrase")
+        ),
         format=cli_args.format,
         output=cli_args.output,
     )
