3e1f25afb8ea9654c6eb5e90db688b8f9330d700
Caleb Evans <caleb@calebevans.me>
2025-12-10 16:28:12 -0800

    Add Bits UI as component library

---
+++ desktop-app/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  const { children } = $props();
+  import '../styles/layout.css';
+</script>
+
+<main>
+  <section>
+    {#if children}
+      {@render children()}
+    {/if}
+    </section>
+</main>
