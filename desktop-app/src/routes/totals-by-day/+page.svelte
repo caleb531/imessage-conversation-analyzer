cb519e93246f0aaae7ede72f49ea6c6c4512eab5
Caleb Evans <caleb@calebevans.me>
2025-12-17 10:55:26 -0800

    Add "Totals by Day" grid

---
+++ desktop-app/src/routes/totals-by-day/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import ResultGrid from '../../components/ResultGrid.svelte';
+</script>
+
+<ResultGrid
+    title="Totals by Day"
+    description="See your message totals, per person or overall, for each day you've messaged"
+    command={['totals_by_day']}
+/>
