509dde5bebdc808f7ec38b03f9e63d68c17123ff
Caleb Evans <caleb@calebevans.me>
2025-12-17 20:45:12 -0800

    Remove obsolete test utilities

--- tests/utils.py
+++ tests/utils.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python3
 """global test fixtures and helper methods"""
 
-import contextlib
-import inspect
-import os
-import os.path
-import shutil
 import tempfile
-import unittest
-from functools import wraps
 from io import BytesIO, StringIO
 from pathlib import Path
-from typing import Any, Callable, Generator
 from unittest import TestCase
 from unittest.mock import patch
-
-from tests.mock_db_utils import create_mock_db
 
 # Disable maximum length of test diff output for all tests (source:
 # <https://stackoverflow.com/a/23617918/560642>)
@@ -33,36 +23,6 @@ mock_chats_db_path = temp_ica_dir / "chat.db"
 chats_db_path_patcher = patch("ica.core.DB_PATH", mock_chats_db_path)
 
 
-class ICATestCase(unittest.TestCase):
-    """A base class from which all other ICA test case classes inherit."""
-
-    def setUp(self) -> None:
-        """
-        Create temporary directory and initialize mock databases for each test.
-        """
-        # If the user aborts the tests with control-C before all tests complete,
-        # it can prevent the teardown code from running, thus causing an error
-        # the next time tests are run because the database tables still exist;
-        # to fix this, we simply run the teardown logic before running the setup
-        # logic (we still run it at the end of every test, as well)
-        self.tearDown()
-        contacts_db_glob_patcher.start()
-        chats_db_path_patcher.start()
-        with contextlib.suppress(OSError):
-            temp_ica_dir.mkdir(parents=True, exist_ok=True)
-        create_mock_db("contacts", mock_contacts_db_path)
-        create_mock_db("chats", mock_chats_db_path)
-
-    def tearDown(self) -> None:
-        """
-        Remove temporary directory and unlink mock databases for each test.
-        """
-        with contextlib.suppress(OSError):
-            shutil.rmtree(temp_ica_dir)
-        chats_db_path_patcher.stop()
-        contacts_db_glob_patcher.stop()
-
-
 class StdoutMockWithBuffer(StringIO):
     """
     A mock class for standard output that provides both text and binary buffers.
@@ -77,38 +37,3 @@ class StdoutMockWithBuffer(StringIO):
     def __init__(self, initial_value: str = "", newline: str = "\n") -> None:
         super().__init__(initial_value, newline)
         object.__setattr__(self, "buffer", BytesIO())
-
-
-class use_env(object):
-    """
-    A decorator (can also be used as a context manager) which sets an
-    environment variable for only the lifetime of the given code; this utility
-    works seamlessly for both functions and generators
-    """
-
-    def __init__(self, key: str, value: str) -> None:
-        self.key: str = key
-        self.value: str = value
-
-    def __enter__(self) -> None:
-        self.orig_value: str = os.environ.get(self.key, "")
-        os.environ[self.key] = self.value
-
-    def __exit__(self, type: type, value: Exception, traceback: Any) -> None:
-        os.environ[self.key] = self.orig_value
-
-    def __call__(self, func: Callable[..., Any]) -> Callable[..., Any]:
-        @wraps(func)
-        def function_wrapper(*args: Any, **kwargs: Any) -> Any:
-            with self:
-                return func(*args, **kwargs)
-
-        @wraps(func)
-        def generator_wrapper(*args: Any, **kwargs: Any) -> Generator[Any, None, Any]:
-            with self:
-                return (yield from func(*args, **kwargs))
-
-        if inspect.isgeneratorfunction(func):
-            return generator_wrapper
-        else:
-            return function_wrapper
